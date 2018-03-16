95487f2fc274efe3f813dee67d27b8ca3d8253fc
Murugan <murugan@firstdraft.com>
2018-03-16 00:27:34 +0000

    done with weather forecast

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -14,6 +14,28 @@ class EventsController < ApplicationController
   def show
     @event = Event.find(params.fetch("id_to_display"))
     
+    #for weather forecast
+    # @street_address = params[:user_street_address]
+    @street_address = @event.city
+    @url_safe_street_address = URI.encode(@street_address)
+
+    @url = "https://maps.googleapis.com/maps/api/geocode/json?address="+@url_safe_street_address.to_s
+    @parsed_result = JSON.parse(open(@url).read)
+    @latitude = @parsed_result.dig("results", 0, "geometry", "location", "lat").to_s
+    @longitude = @parsed_result.dig("results", 0, "geometry", "location", "lng").to_s
+
+    @url2="https://api.darksky.net/forecast/2c822d92f31656e2262fa7f6429ef27a/"+@latitude.to_s+","+@longitude.to_s
+    @parsed_data = JSON.parse(open(@url2).read)
+    
+    @current_temperature = @parsed_data.dig("currently", "temperature").to_s
+
+    @current_summary = @parsed_data.dig("currently", "summary").to_s
+
+    @summary_of_next_sixty_minutes = @parsed_data.dig("minutely", "summary").to_s
+
+    @summary_of_next_several_hours = @parsed_data.dig("hourly", "summary").to_s
+
+    @summary_of_next_several_days = @parsed_data.dig("daily", "summary").to_s
   
     render("event_templates/show.html.erb")
   end
